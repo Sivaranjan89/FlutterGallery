02e6b84f9d35557d46b477531c31c4df73defba4
sivaranjan89 <lsivaranjan@gmail.com>
2019-09-26 19:28:53 +0530

    Removed TODO

--- lib/datatable/paginated_data_table.dart
+++ lib/datatable/paginated_data_table.dart
@@ -206,15 +206,12 @@ class FamilyDataSource extends DataTableSource {
   }
 
   @override
-  // TODO: implement isRowCountApproximate
   bool get isRowCountApproximate => false;
 
   @override
-  // TODO: implement rowCount
   int get rowCount => users.length;
 
   @override
-  // TODO: implement selectedRowCount
   int get selectedRowCount => selectedCount;
 
 
